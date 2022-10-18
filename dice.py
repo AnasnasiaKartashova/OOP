97723bfa501e50ab10a93d769e34d3bbcd7d765b
Anastasia <86727119+AnasnasiaKartashova@users.noreply.github.com>
2022-10-18 18:35:51 +0300

    Update dice.py

--- dice.py
+++ dice.py
@@ -2,24 +2,21 @@
 
 from random import choice
 
+
 class Dice:
 
-   def __init__(self, *list_faced):
-        self.list_faced = list(list_faced)
+    def __init__(self, list_faced):
+        list_faced = list(range(1, list_faced + 1))
+        self.list_faced = list_faced
 
-   def roll_dice(self):
+    def roll_dice(self):
         roll = choice(self.list_faced)
         print("На кубике выпало:", roll)
         return roll
 
-dice_1 = Dice('1', '2', '3', '4', '5', '6','7','8','9',
-               '10','11','12','13','14','15','16','17',
-               '18','19','20')
 
+dice_1 = Dice(20)
 dice_1.roll_dice()
-
-
-
 
 
 
